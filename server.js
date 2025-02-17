b1994b41cfdee7e5702b3f94a45500e2e11ce02f
JakeKing0001 <davidepesino5806@gmail.com>
2025-02-17 10:09:11 +0100

    added broadcast

--- server.js
+++ server.js
@@ -8,12 +8,15 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+// Broadcast to all clients
+wss.broadcast = (data) => wss.clients.forEach((client) => client.readyState === WebSocket.OPEN && client.send(data));
+
 wss.on("connection", (ws) => {
   console.log("✅ Client connesso");
 
   ws.on("message", (message) => {
     console.log("📩 Messaggio ricevuto:", message);
-    ws.send(`ECO: ${message}`);
+    wss.broadcast(`ECO: ${message}`);
   });
 
   ws.on("close", () => console.log("❌ Client disconnesso"));
